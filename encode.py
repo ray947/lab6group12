db77e02a4cfbc62a43063aa808eb1645977715f2
aahanshin <159496047+ray947@users.noreply.github.com>
2024-10-23 20:55:31 -0400

    first commit

---
+++ encode.py
@@ -0,0 +1,40 @@
+#aahan shin
+
+def encode(password):
+    encoded_password = ""
+    for digit in password:
+        new_digit = (int(digit) + 3) % 10 
+        encoded_password += str(new_digit)
+    return encoded_password
+
+
+
+def main():
+    encoded_password = ""  
+    while True:
+        print("\nMenu")
+        print("------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        
+        option = input("Please enter an option: ")
+
+        if option == "1":
+            password = input("Please enter your password to encode: ")
+            if len(password) == 8 and password.isdigit():  
+                encoded_password = encode(password)
+                print("Your password has been encoded and stored!")
+            else:
+                print("error")
+        
+        
+        
+        elif option == "3":
+            break  
+
+        else:
+            print("error")
+
+if __name__ == "__main__":
+    main()
